f71e3b7d6bc36af66ca09acb1b18733a414a1609
Karolina Roszkowska <inmatrixos@gmail.com>
2020-04-11 20:39:02 +0100

    new console app that sends get request and receives response from API

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ConsoleApp_AspNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetData();
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+        static async void GetData()
+        {
+            //Make a GET request
+            HttpClient client = new HttpClient();
+            //Receive a GET response
+            HttpResponseMessage response = await client
+                .GetAsync("https://www.dallasopendata.com/resource/7e48-gxg9.json");
+            response.EnsureSuccessStatusCode();
+            string data = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(data);
+        }
+    }
+}
